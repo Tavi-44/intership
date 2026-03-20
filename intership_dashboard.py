733b3b3a51d447187a455acaccddd4e9694b4571
Tanmay Virkar <tanmayvirkar691@gmail.com>
2026-03-20 11:24:53 +0530

    Update intership_dashboard.py

--- intership_dashboard.py
+++ intership_dashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import gdown
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,8 +7,11 @@ st.set_page_config(page_title="Trader Performance Dashboard", layout="wide")
 st.title("📊 Trader Performance vs Market Sentiment")
 @st.cache_data
 def load_data():
-    url = "https://drive.google.com/uc?id=1T3oKb46tkYlXOTEUrY3UvoAcPIYZy6g7"
-    df = pd.read_csv(url)
+    file_id = "1T3oKb46tkYlXOTEUrY3UvoAcPIYZy6g7"
+    url = f"https://drive.google.com/uc?id={file_id}"
+    output = "merged_data.csv"
+    gdown.download(url, output, quiet=False)
+    df = pd.read_csv(output)
     return df
 df = load_data()
 df = df.dropna(subset=['sentiment'])
